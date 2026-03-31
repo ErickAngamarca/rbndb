d20941d28891473405faf111d2c07c96cda7ff41
ErickAngamarca <erick.angamarca97@gmail.com>
2026-03-31 14:36:46 -0500

    Replace leaflet with simple plot() map
    - Changed map to use base R plot() instead of leaflet
    - Removed leaflet dependency from DESCRIPTION
    - Added graphics imports for points and plot
    - Updated documentation and README

---
+++ rbndb.Rcheck/rbndb-Ex.R
@@ -0,0 +1,53 @@
+pkgname <- "rbndb"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+library('rbndb')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+base::assign(".old_wd", base::getwd(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("download_bndb")
+### * download_bndb
+
+flush(stderr()); flush(stdout())
+
+### Name: download_bndb
+### Title: Download occurrence data from BNDB Ecuador
+### Aliases: download_bndb rbndb
+
+### ** Examples
+
+## Not run: 
+##D # Download all records for a species (all Ecuador)
+##D occ <- download_bndb("Cedrela odorata")
+##D 
+##D # Download records filtered by a shapefile
+##D occ <- download_bndb("Alnus acuminata", polygon = "path/to/polygon.shp", crs = "EPSG:4326")
+##D 
+##D # Save as CSV
+##D download_bndb("Cedrela odorata", polygon = "path/to/polygon.shp", 
+##D               output = "csv", out_file = "data")
+##D 
+##D # Save as shapefile in UTM (crs required)
+##D download_bndb("Cedrela odorata", polygon = "path/to/polygon.shp", 
+##D               output = "shp", out_file = "data", crs = "EPSG:32717")
+##D 
+##D # Display interactive map (crs required)
+##D download_bndb("Alnus acuminata", polygon = "path/to/polygon.shp", 
+##D               map = TRUE, crs = "EPSG:4326")
+## End(Not run)
+
+
+
+### * <FOOTER>
+###
+cleanEx()
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
